7903834bc65b19ecf19efbaff80cb2f4430d552d
marcelocarmona <marcelo@marcelocarmona.com>
2023-04-07 14:31:55 -0700

    show header title in mobile

--- components/LayoutWrapper.js
+++ components/LayoutWrapper.js
@@ -19,7 +19,7 @@ const LayoutWrapper = ({ children }) => {
                   <Logo />
                 </div>
                 {typeof siteMetadata.headerTitle === 'string' ? (
-                  <div className="hidden h-6 text-2xl font-semibold sm:block">
+                  <div className="h-6 text-xl font-semibold sm:block sm:text-2xl">
                     {siteMetadata.headerTitle}
                   </div>
                 ) : (
