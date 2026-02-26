e2f6ba710852a2b795feabffac17b09f23639a1c
Marcelo Carmona <marcelo@marcelocarmona.com>
2026-02-26 02:59:49 -0800

    Fix feed.xml route conflicts and git ignore rules

---
+++ app/feed.xml/route.js
@@ -0,0 +1,15 @@
+import generateRss from '@/lib/generate-rss'
+import { getAllFilesFrontMatter } from '@/lib/mdx'
+
+export const runtime = 'nodejs'
+
+export async function GET() {
+  const posts = await getAllFilesFrontMatter('blog')
+  const rss = generateRss(posts)
+
+  return new Response(rss, {
+    headers: {
+      'Content-Type': 'application/rss+xml; charset=utf-8',
+    },
+  })
+}
